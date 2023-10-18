18bf9459fc303451bb54f78c8669a46c4018183b
Bhargav-Synfosys <110816674+Bhargav-Synfosys@users.noreply.github.com>
2023-10-18 17:22:53 +0530

    first commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => console.log('Done')}>
+        Add To Cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
